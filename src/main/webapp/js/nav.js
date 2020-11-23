fcf40650bbac3f7a9219793c9b4719f080ed062e
Peter Chapin <chapinp@acm.org>
2020-11-23 13:26:57 -0500

    Adjusted some of the JavaScript files.

--- src/main/webapp/js/nav.js
+++ src/main/webapp/js/nav.js
@@ -1,1 +1,35 @@
-function toggle(a,b){var c=document.getElementById(a);"none"!=c.style.display?(c.style.display="none",document.getElementById(b).setAttribute("aria-expanded","false")):(c.style.display="inline-block",document.getElementById(b).setAttribute("aria-expanded","true"))}function mouseEnterBasicLEO(){document.getElementById("BasicLEO").style.display="inline-block",document.getElementById("drop1").setAttribute("aria-expanded","true"),setTimeout(mouseLeaveBasicLEO,dropdownTimeout)}function mouseLeaveBasicLEO(){document.getElementById("BasicLEO").style.display="none",document.getElementById("drop1").setAttribute("aria-expanded","false")}function mouseEnterAlaskanIce(){document.getElementById("AlaskanIceDropdown").style.display="inline-block",document.getElementById("drop2").setAttribute("aria-expanded","true"),setTimeout(mouseLeaveAlaskanIce,dropdownTimeout)}function mouseLeaveAlaskanIce(){document.getElementById("AlaskanIceDropdown").style.display="none",document.getElementById("drop2").setAttribute("aria-expanded","false")}var dropdownTimeout=6e3;
+function toggle(a, b)
+{
+    var c = document.getElementById(a);
+    "none" !== c.style.display ?
+        (c.style.display="none", document.getElementById(b).setAttribute("aria-expanded","false")) :
+        (c.style.display="inline-block", document.getElementById(b).setAttribute("aria-expanded","true"));
+}
+
+function mouseEnterBasicLEO()
+{
+    document.getElementById("BasicLEO").style.display="inline-block",
+    document.getElementById("drop1").setAttribute("aria-expanded","true"),
+    setTimeout(mouseLeaveBasicLEO,dropdownTimeout);
+}
+
+function mouseLeaveBasicLEO()
+{
+    document.getElementById("BasicLEO").style.display="none",
+    document.getElementById("drop1").setAttribute("aria-expanded","false");
+}
+
+function mouseEnterAlaskanIce()
+{
+    document.getElementById("AlaskanIceDropdown").style.display="inline-block",
+    document.getElementById("drop2").setAttribute("aria-expanded","true"),
+    setTimeout(mouseLeaveAlaskanIce,dropdownTimeout);
+}
+
+function mouseLeaveAlaskanIce()
+{
+    document.getElementById("AlaskanIceDropdown").style.display="none",
+    document.getElementById("drop2").setAttribute("aria-expanded","false");
+}
+
+var dropdownTimeout=6e3;
--- src/main/webapp/js/nav_edit.js
+++ src/main/webapp/js/nav_edit.js
@@ -1,41 +1,48 @@
 /*
-	Document   : nav.js
-    Created on : Nov 12, 2019, 1:35:33 PM
-    Author     : Vermont Technical College
+ * Document   : nav.js
+ * Created on : Nov 12, 2019, 1:35:33 PM
+ * Author     : Vermont Technical College
+ * 
+ * Note: This is the uncompressed copy of nav.js.
+ * (The compression was silly (my opinion), so I reformatted nav.js. Why have both? --pchapin)
+ */
+var dropdownTimeout = 6000; //ms
 
-   Note: This is the uncompressed copy of nav.js
-*/
-var dropdownTimeout = 6000;//ms
-
-function toggle(id, id2){
-	var n = document.getElementById(id);
-	if (n.style.display != 'none'){
-	  n.style.display = 'none';
-	  document.getElementById(id2).setAttribute('aria-expanded', 'false');
-	}else{
-	  n.style.display = 'inline-block';
-	  document.getElementById(id2).setAttribute('aria-expanded', 'true');
-	}
+function toggle(id, id2)
+{
+  var n = document.getElementById(id);
+  if (n.style.display !== 'none') {
+    n.style.display = 'none';
+    document.getElementById(id2).setAttribute('aria-expanded', 'false');
+  }
+  else {
+    n.style.display = 'inline-block';
+    document.getElementById(id2).setAttribute('aria-expanded', 'true');
+  }
 }
 
-function mouseEnterBasicLEO(){
-	document.getElementById("BasicLEO").style.display = 'inline-block';
-	document.getElementById("drop1").setAttribute('aria-expanded', 'true');
-	setTimeout(mouseLeaveBasicLEO, dropdownTimeout);
+function mouseEnterBasicLEO()
+{
+  document.getElementById("BasicLEO").style.display = 'inline-block';
+  document.getElementById("drop1").setAttribute('aria-expanded', 'true');
+  setTimeout(mouseLeaveBasicLEO, dropdownTimeout);
 }
 
-function mouseLeaveBasicLEO(){
-	document.getElementById("BasicLEO").style.display = 'none';
-	document.getElementById("drop1").setAttribute('aria-expanded', 'false');
+function mouseLeaveBasicLEO()
+{
+  document.getElementById("BasicLEO").style.display = 'none';
+  document.getElementById("drop1").setAttribute('aria-expanded', 'false');
 }
 
-function mouseEnterAlaskanIce(){
-	document.getElementById("AlaskanIceDropdown").style.display = 'inline-block';
-	document.getElementById("drop2").setAttribute('aria-expanded', 'true');
-	setTimeout(mouseLeaveAlaskanIce, dropdownTimeout);
+function mouseEnterAlaskanIce()
+{
+  document.getElementById("AlaskanIceDropdown").style.display = 'inline-block';
+  document.getElementById("drop2").setAttribute('aria-expanded', 'true');
+  setTimeout(mouseLeaveAlaskanIce, dropdownTimeout);
 }
 
-function mouseLeaveAlaskanIce(){
-	document.getElementById("AlaskanIceDropdown").style.display = 'none';
-	document.getElementById("drop2").setAttribute('aria-expanded', 'false');
+function mouseLeaveAlaskanIce()
+{
+  document.getElementById("AlaskanIceDropdown").style.display = 'none';
+  document.getElementById("drop2").setAttribute('aria-expanded', 'false');
 }
